5e5c20b56f5651a1557e1b91526034e9ba170a00
Nacim <nacim.msallem@gmail.com>
2024-04-07 23:06:59 +0100

    admin/session design

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -2,33 +2,43 @@ import Link from "next/link";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
 
-
 const Links = () => {
-const links = [
-  {
-    title: "Homepage",
-    path: "/",
-  },
-  {
-    title: "About",
-    path: "/about",
-  },
-  {
-    title: "Contact",
-    path: "/contact",
-  },
-  {
-    title: "Blog",
-    path: "/blog",
-  },
-];
+  const links = [
+    {
+      title: "Homepage",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+  ];
+  const session = true;
+  const isAdmin = true;
 
-return (
+  return (
     <div className={styles.links}>
-    {links.map((link) => (
-          <NavLink item={link} key={link.title} />
-        ))}
-  </div>
-);
-}
+      {links.map((link) => (
+        <NavLink item={link} key={link.title} />
+      ))}
+
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "login", path: "/login" }} />
+      )}
+    </div>
+  );
+};
 export default Links;
