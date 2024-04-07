7db457b3cb3799fa34ecb288fa378a51f25e9cc3
Nacim <nacim.msallem@gmail.com>
2024-04-07 15:41:34 +0100

    first commit

---
+++ src/app/error.jsx
@@ -0,0 +1,8 @@
+"use client" //has to be client side
+const Error = ()=> {
+    return (
+
+        <div>Error</div>
+    )
+}
+export default Error
---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const NotFound = () => {
+    return( <div>
+        <h2>This Page Is Not Found</h2>
+    <Link href="/"> Return Home</Link>
+    </div>
+    )
+  };
+  
+  export default NotFound;
